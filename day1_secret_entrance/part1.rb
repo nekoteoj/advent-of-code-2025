36785db6d5021ac7979de6a94a21265488c54028
Pisit Wajanasara <compisit1234@gmail.com>
2025-12-02 00:28:39 -0800

    Update day1 code to handle file close

--- day1_secret_entrance/part1.rb
+++ day1_secret_entrance/part1.rb
@@ -6,11 +6,13 @@ position = 50
 bound = 100
 password = 0
 
-File.open(input_path, 'r').each_line.map(&:strip).each do |line|
-  sign = line[0] == 'L' ? -1 : 1
-  amount = line[1..].to_i
-  position = (((position + sign * amount) % bound) + bound) % bound
-  password += 1 if position.zero?
+File.open(input_path, 'r') do |file|
+  file.each_line.map(&:strip).each do |line|
+    sign = line[0] == 'L' ? -1 : 1
+    amount = line[1..].to_i
+    position = (((position + sign * amount) % bound) + bound) % bound
+    password += 1 if position.zero?
+  end
 end
 
 puts password
--- day1_secret_entrance/part2.rb
+++ day1_secret_entrance/part2.rb
@@ -6,16 +6,18 @@ position = 50
 bound = 100
 password = 0
 
-File.open(input_path, 'r').each_line.map(&:strip).each do |line|
-  sign = line[0] == 'L' ? -1 : 1
-  amount = line[1..].to_i
-  adjusted_position = amount + case sign
-                               when 1 then position
-                               else ((bound - position) % bound)
-                               end
-  password += (adjusted_position / bound).abs
-  position += sign * amount
-  position = ((position % bound) + bound) % bound
+File.open(input_path, 'r') do |file|
+  file.each_line.map(&:strip).each do |line|
+    sign = line[0] == 'L' ? -1 : 1
+    amount = line[1..].to_i
+    adjusted_position = amount + case sign
+                                 when 1 then position
+                                 else ((bound - position) % bound)
+                                 end
+    password += (adjusted_position / bound).abs
+    position += sign * amount
+    position = ((position % bound) + bound) % bound
+  end
 end
 
 puts password
