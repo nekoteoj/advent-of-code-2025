887d590834fd0b4adfb653401e66cb04fbcec001
Pisit Wajanasara <compisit1234@gmail.com>
2025-12-01 23:23:20 -0800

    Add day 1 secret entrance solution

---
+++ day1_secret_entrance/part1.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+input_path = ARGV.first
+
+position = 50
+bound = 100
+password = 0
+
+File.open(input_path, 'r').each_line.map(&:strip).each do |line|
+  sign = line[0] == 'L' ? -1 : 1
+  amount = line[1..].to_i
+  position = (((position + sign * amount) % bound) + bound) % bound
+  password += 1 if position.zero?
+end
+
+puts password
---
+++ day1_secret_entrance/part2.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+input_path = ARGV.first
+
+position = 50
+bound = 100
+password = 0
+
+File.open(input_path, 'r').each_line.map(&:strip).each do |line|
+  sign = line[0] == 'L' ? -1 : 1
+  amount = line[1..].to_i
+  adjusted_position = amount + case sign
+                               when 1 then position
+                               else ((bound - position) % bound)
+                               end
+  password += (adjusted_position / bound).abs
+  position += sign * amount
+  position = ((position % bound) + bound) % bound
+end
+
+puts password
